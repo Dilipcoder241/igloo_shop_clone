99e47fcc94611e708558890c4ac9ad2d7fdb002f
dilip coder <mihirsingh241@gmail.com>
2023-08-11 19:56:31 +0530

    small animation change

--- script.js
+++ script.js
@@ -3,13 +3,8 @@ var tl =gsap.timeline();
 
 tl.from("#nav", {
     y:-100,
-    duration:0.7,
+    duration:1,
 })
-
-
-// gsap.from(".fourth img",{
-//     x:-400,
-//     duration:20,
-//     repeat:-1,
-//     yoyo:true,
-// })
+tl.from(".bg video" , {
+    scale:0,
+})
